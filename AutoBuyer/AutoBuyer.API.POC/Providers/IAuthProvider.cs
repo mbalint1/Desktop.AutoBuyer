cf475927084481fbd0b5b5770da14081e2ccd9b1
Michael <mbalint1@gmail.com>
2020-03-08 09:10:37 -0500

    Removed the API proof of concept project. Moving it to a different solution.

--- AutoBuyer/AutoBuyer.API.POC/Providers/IAuthProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutoBuyer.API.POC.Models;
-
-namespace AutoBuyer.API.POC.Providers
-{
-    public interface IAuthProvider
-    {
-        AuthResponse Authenticate(string user, string password);
-    }
-}
