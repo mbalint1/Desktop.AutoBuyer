f5f057cc2fbd09071a463e8108bf6ef4858ddd8f
Michael Balint <mbalint@ghtulsa.com>
2020-09-30 16:23:38 -0500

    More calibration

--- AutoBuyer/AutoBuyer.Core/Controllers/ScreenController.cs
+++ AutoBuyer/AutoBuyer.Core/Controllers/ScreenController.cs
@@ -278,7 +278,7 @@ namespace AutoBuyer.Core.Controllers
 
             results.Dispose();
 
-            return percentMatch >= 90;
+            return percentMatch >= 92;
         }
 
         public bool SuccessfulPurchase()
